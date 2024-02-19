e1110af05d0dcc20325472ab2ca8178999f42f76
Aditya2503200 <137478574+Aditya2503200@users.noreply.github.com>
2024-02-19 22:34:24 +0530

    Create first.cpp

---
+++ first.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int a,b,c;
+  cout<<"Aditya Bansal ----->21BCS11682"<<endl;
+  cout<<"Enter the two numbers to add"<<endl;
+  cin>>a>>b;
+  c = a+b;
+  cout<<"The sum of "<<a<<" and "<<b<<" is "<<c<<endl;
+  return 0;
+}
